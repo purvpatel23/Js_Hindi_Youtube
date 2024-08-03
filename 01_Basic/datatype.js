31277929860e02fd25f89edfcba688b5e2c8ce58
purvpatel23 <125570930+purvpatel23@users.noreply.github.com>
2024-08-03 12:32:17 +0000

    datatypes

---
+++ 01_Basic/datatype.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as newer version
+// alert("hello") // we are using nodejs , not browser 
+
+// console.log("purv") ; console.log("patel")  // not write like this, code readablity should be high 
+
+//  code format 
+console.log("purv")
+console.log("patel")
+
+let name = "purv"
+let age = 13
+let isloggedin = false
+let  state = null  // decribe null like this in string. 
+
+
+////// this all are primative datatypes 
+
+//number => 2 to power 53
+// bigint => use for big number 
+// string => preffer " "
+// boolean => true or false 
+// null => standalone value 
+// undefine => not declare value 
+// symbol => use for unique 
+
+// object
+
+console.log(typeof age )
+console.log(typeof undefined )
+console.log(typeof null)
