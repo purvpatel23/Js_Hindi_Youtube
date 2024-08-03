4b2acdad1ca3dd147b8dea4d5f66ce1194c64b7c
purvpatel23 <125570930+purvpatel23@users.noreply.github.com>
2024-08-03 13:07:14 +0000

    conversion

---
+++ 01_Basic/conversionOpration.js
@@ -0,0 +1,23 @@
+let score = "string"
+console.log(typeof score);
+let valueinnumber = Number(score); // convert string to number 
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+let isloggedIn = ""
+let booleanislogggedIn = Boolean(isloggedIn)
+
+console.log(booleanislogggedIn)
+
+// 1 => true; 0 => false 
+// "" => false
+// "hitesh" => true
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(typeof stringnumber);
