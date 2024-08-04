188b2c8a26e17f895106c300e23af9c39d41c73b
purvpatel23 <125570930+purvpatel23@users.noreply.github.com>
2024-08-04 11:26:32 +0000

    comparision and opration

---
+++ 01_Basic/opration.js
@@ -0,0 +1,25 @@
+ let value = 3 ;
+let nagvalue = -value;
+// console.log(nagvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+
+// let str1 = "hello"
+// let str2 = " purv"
+
+// let str3 = str1 + str2;
+// console.log(str3);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// don't write this type confused , use a peranthesis in opration code
+
+// console.log(+"");
+
+let gamecounter = 100
+++gamecounter
+console.log(gamecounter);
+
