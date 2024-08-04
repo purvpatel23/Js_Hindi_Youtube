3a6c8172cdeb4d11c01bdd83f24b958c88e3f615
purvpatel23 <125570930+purvpatel23@users.noreply.github.com>
2024-08-04 16:46:42 +0000

    summrydatatype

---
+++ 01_Basic/datatypesummery.js
@@ -0,0 +1,42 @@
+//primative 
+// 7 types : string , number, boolean , null , undefined ,symbol , Bigint
+
+// --------------------------------------------------------------------------------
+
+// Reference (Non Primative)
+//  Array , Objacts , Functions 
+
+// const score = 100 
+// const scorevalue = 100.3
+// const isloogin = false
+// const outsideTemp = null
+// let useEmail ; 
+// const id = Symbol('123')
+// const anotherid = Symbol('123')
+
+// console.log(id === anotherid)
+
+// const bignumber = 13n
+
+// console.log(typeof bignumber)
+
+const heros = ["superman", "shaktiman", "raju"]
+let myobj = {
+    name: "purv",
+    age: 22,
+}
+
+const myfunction = function(){
+ console.log("hello world");
+
+}
+
+console.log(typeof myfunction)
+console.log(typeof heros)
+
+// "abc" = string 
+// 123 = number
+// ["a","b","c"] = array
+// function = function
+// null = object
+// object = object 
