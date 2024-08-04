0746c80aeb2f9e5656e768d8e3687eb1747ea1f1
purvpatel23 <125570930+purvpatel23@users.noreply.github.com>
2024-08-04 17:15:35 +0000

    memory in stack and heap

---
+++ 01_Basic/stack and heap.js
@@ -0,0 +1,25 @@
+// two types of memory  stack(primative) and heap(non-primative) 
+
+
+
+// let myyoutube = "purv@gmail.com"
+// let anothername = myyoutube
+// anothername = "patel@gmail.com"
+
+// console.log(myyoutube);
+// console.log(anothername);
+
+//  in primative when we create new variable then we can't change orignal value 
+
+//  in non-primative orignal value can change 
+let userone = {
+    email:"patelpurv@gmail.com",
+    password: 123
+
+}
+
+let usertwo = userone
+usertwo.email = "pp@gmail.com"
+
+console.log(usertwo);
+
