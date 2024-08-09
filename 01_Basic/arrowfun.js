1e3c0f0e26b5e6f5dc97886f8a8e0f85b8bc0659
purvpatel23 <125570930+purvpatel23@users.noreply.github.com>
2024-08-09 10:20:17 +0000

    arrow

--- 01_Basic/arrowfun.js
+++ 01_Basic/arrowfun.js
@@ -1,56 +1,49 @@
-// const user = {
-//  name: "purv",
-//  price: 999,
+const user = {
+name: "purv",
+price: 999,
+welcomemessage: function(){
+     console.log(`${this.name} ,welcome to website`);
+     console.log(this);
+  }
+}
+user.welcomemessage()
+user.name = "pam";
+user.welcomemessage()
 
-//   welcomemessage: function(){
-//      console.log(`${this.name} ,welcome to website`);
-//      console.log(this);
-     
-//   }
+console.log(); //-----return {}
+console.log(this); //----return undefined
 
-// }
-// user.welcomemessage()
-// user.name = "pam";
-// user.welcomemessage()
-
-// console.log();
-
-// console.log(this);
-
-// function chai(){
-//     let username = "purv"
-//     console.log(this);
+function chai(){
+    let username = "purv"
+    console.log(this);
 
         
-// }
-// chai()
+}
+chai()
 
-// const chai = function (){
-//     let username = "purv"
-//     console.log(this.username);
+const chai = function (){
+    let username = "purv"
+    console.log(this.username);
     
-// }
-// chai()
+}
+chai()
 
-// const chai =  () =>{
-// let username = "purv"
-// console.log(this.username);
+const chai =  () =>{
+let username = "purv"
+console.log(this.username);
 
-// }
-// chai()
+}
+chai()
 // never use this in function it is only use in object.
 
 const addtwo = (num1 , num2) => {
    return num1 + num2
-
 }  // arrow function 
+console.log(addtwo(2,3)); //  //explicit function  because it is use a return.
 
+// const addthree = (num1 , num2) => num1 + num2  //implicit function is use when we don't need to use a return in function.
+// console.log(addthree(3,5));
 
-console.log(addtwo(2,3));
-
-
-const addthree = (num1 , num2) => num1 + num2  //implesit function 
-
-console.log(addthree(3,5));
-
- 
+// when i need return in object--------------------------------------------
+const addfour = (num1 , num2) => ({username: "purv"})
+console.log(addfour());
