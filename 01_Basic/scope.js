c3f2fc00c7573a23fe9ce8f766f94884b05a7fa7
purvpatel23 <125570930+purvpatel23@users.noreply.github.com>
2024-08-09 06:42:47 +0000

    scope

---
+++ 01_Basic/scope.js
@@ -0,0 +1,28 @@
+if (true) {
+    const username = "purv"
+    if(username === "purv")
+    {
+        const website = "youtube"
+        console.log(username + website);
+        console.log(website);
+    }    
+    console.log(username);
+    
+}
+
+
+//--------------------------------interesting-------------------------
+function addone(num){
+ return num +1
+} 
+
+const result = addone(5)
+console.log(result);
+
+// second way 
+
+const addtwo = function (num){
+    return num+2
+}
+addtwo(5)
+
